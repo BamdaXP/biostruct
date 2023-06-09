edf3a5be6c1c53642b23c7b02bb23ac1132c7772
BamdaXP <wanxuanjun@sina.cn>
2023-06-09 17:48:30 +0800

    fix

--- components/fix.py
+++ components/fix.py
@@ -1,6 +1,7 @@
-import os
+import os,re
 
-import biostruct as bs
+from biostruct.entry import Entry
+
 import pandas as df
 
 import modeller 
@@ -13,29 +14,50 @@ import Bio.PDB.Chain
 from Bio.PDB.MMCIFParser import MMCIFParser
 
 
-def get_fix(entry:bs.Entry,chain_id:str,redo=False)-> Bio.PDB.Chain.Chain:
+def get_fix(entry:Entry,chain_id:str,redo=False)-> Bio.PDB.Chain.Chain:
 	#Make fix dir
 	os.makedirs(entry.get_path('fix/'),exist_ok=True)
 	fix_file = entry.get_path(f'fix/{chain_id}.cif')
-	if os.path.isfile(fix_file) and os.path.getsize(fix_file):
+	if not (os.path.isfile(fix_file) and os.path.getsize(fix_file)) or redo:
 		#Export sequence fasta file
 		fasta_file = entry.get_path(f'fix/{chain_id}.fasta')
 		record = SeqRecord(Seq(entry.fasta_dict[chain_id]['sequence']),
-							id="",
-							name="",
-							description=entry.fasta_dict[chain_id]['description'])
+							id="Sequence",
+							name='',
+							description='')
 		Bio.SeqIO.write(record, fasta_file, "fasta")
 
 		print(f"Fixing cif file chain {chain_id} in {entry}")
 		env = modeller.Environ()
-		m = modeller.Model(env,file="mmcif.cif",model_format='MMCIF')
-		aln =modeller.alignment(env)
-		aln.append(file=fasta_file, align_codes='all')
-		a = modeller.automodel.automodel(env,alnfile=aln,knowns=m,sequence=fasta_file)
+
+		m = modeller.Model(env,file=entry.get_path("mmcif.cif"),model_format='MMCIF', model_segment=(f'FIRST:{chain_id}',f'LAST:{chain_id}'))
+		aln =modeller.Alignment(env)
+		aln.append(file=fasta_file, align_codes="Sequence",alignment_format='FASTA')
+		aln.append_model(mdl=m, align_codes="Structure",atom_files=entry.get_path("mmcif.cif"))
+		aln.align2d()
+		aln.write(file=entry.get_path(f'fix/{chain_id}.pir'),alignment_format='PIR')
+		#Clean \n in pir headers
+		s = ''
+		with open(entry.get_path(f'fix/{chain_id}.pir'),'r') as f:
+			s = f.read()
+			s = ':'.join([[parts[0]] + [part if '>P1;' in part else part.replace('\n', ' ')
+		                  for part in parts[1:-1]] + [parts[-1]] for parts in [s.split(':')]][0])
+		with open(entry.get_path(f'fix/{chain_id}.pir'),'w') as f:
+			f.write(s)
+
+		a = modeller.automodel.AutoModel(env, alnfile=entry.get_path(f'fix/{chain_id}.pir'), knowns="Structure", sequence="Sequence",
+		root_name=f"{entry}_{chain_id}")
 		a.starting_model = 1
 		a.ending_model = 1
-		a.chains = chain_id
 		a.make()
-		a.restraints.write(file=fix_file) #type:ignore
+		a.write(entry.get_path(f'fix/{chain_id}.cif'),model_format='MMCIF')
 
-	return MMCIFParser(QUIET=True).get_structure(entry.pid,fix_file)[0][chain_id]
+		#delete cache files
+		os.remove(f"{a.root_name}.ini")
+		os.remove(f"{a.root_name}.rsr")
+		os.remove(f"{a.root_name}.sch")
+		os.remove(f"{a.root_name}.D00000001")
+		os.remove(f"{a.root_name}.B99990001.pdb")
+		os.remove(f"{a.root_name}.V99990001")
+
+	return MMCIFParser(QUIET=True).get_structure(entry.pid,fix_file)[0]["A"]#The generated cif file would change chain id to A
--- components/msa.py
+++ components/msa.py
@@ -22,20 +22,20 @@ def get_msa(entry:Entry, chain_id:str, db:str, ncpu=4, redo=False):
 	
 	#required files
 	database = f"/mnt/data1/Common_databases/database/{db}/{db}.fasta"
-	sto_filepath = entry.get_path(f'msa/{db}/{chain_id}.msa')
+	sto_filepath = entry.get_path(f'msa/{db}/{chain_id}.sto')
 	mask_filepath = entry.get_path(f'msa/{db}/{chain_id}.msk')
 	msa_filepath = entry.get_path(f'msa/{db}/{chain_id}.msa.fasta')
 	#load if exist
 	#not exist: calculate and clear caches
-	if not (os.path.exists(msa_filepath) and os.path.getsize(msa_filepath)) or redo:
+	if not (os.path.isfile(msa_filepath) and os.path.getsize(msa_filepath)) or redo:
 		#seach if not exist
-		if not(os.path.exists(sto_filepath) and os.path.getsize(sto_filepath)) or redo:
+		if not(os.path.isfile(sto_filepath) and os.path.getsize(sto_filepath)) or redo:
 			print(f"Searching msa {entry}:{chain_id}")
 			cmd = f"jackhmmer --cpu {ncpu}  --noali -A \"{sto_filepath}\" \"{fasta_filepath}\" \"{database}\" > /dev/null"
 			#cmd = f"jackhmmer -N 5 --cpu {ncpu}  --noali --incT {len(entry.fasta_dict[chain_id]['sequence'])*0.5} -A \"{sto_filepath}\" \"{fasta_filepath}\" \"{database}\" > /dev/null"
 			#run
 			os.system(cmd)
-		if not(os.path.exists(mask_filepath) and os.path.getsize(mask_filepath)) or redo:
+		if not (os.path.isfile(mask_filepath) and os.path.getsize(mask_filepath)) or redo:
 			print(f"Masking msa {entry}:{chain_id}")
 			cmd = f"esl-alimask --rf-is-mask \"{sto_filepath}\">\"{mask_filepath}\""
 			os.system(cmd)
@@ -45,8 +45,8 @@ def get_msa(entry:Entry, chain_id:str, db:str, ncpu=4, redo=False):
 		cmd = f"esl-reformat a2m \"{mask_filepath}\">\"{msa_filepath}\""
 		os.system(cmd)
 		#completed, clear caches
-		os.remove(fasta_filepath)
+		#os.remove(fasta_filepath)
 		os.remove(sto_filepath)
 		os.remove(mask_filepath)
 
-	return SeqIO.parse(msa_filepath,'fasta')
+	return #SeqIO.parse(msa_filepath,'fasta')
